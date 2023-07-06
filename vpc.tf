a167a1031bcaf867698df989079d488c8339d274
basedhacker1337 <125813721+basedhacker1337@users.noreply.github.com>
2023-07-06 19:47:23 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 # Defines the VPC, subnets and IP addresses
 
-resource "aws_vpc" "main_vpc" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
+resource "aws_vpc" "main_vpc" {       # Defines the VPC configuration, where subnets will reside.
+    cidr_block = "10.0.0.0/16"        # Defines number of hosts that can fit in the network.
+    instance_tenancy = "default"      # Ensures that EC2 instances launched in this VPC use the EC2 instance tenancy attribute specified when the EC2 instance is launched.
     enable_dns_support = "true"
     enable_dns_hostnames = "false"
 
@@ -11,37 +11,73 @@ resource "aws_vpc" "main_vpc" {
     }
   }
 
-resource "aws_subnet" "recon_subnet" {
+resource "aws_subnet" "recon_subnet" {      # Defines the subnet which the recon machine will reside in.
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "false"
+    map_public_ip_on_launch = "false"       # Specifies that the instances launched into this subnet should not be assigned a public IP address.
 
     tags = {
       Name = "recon subnet"
     }
 }
 
-resource "aws_subnet" "proxy_subnet" {
+resource "aws_subnet" "proxy_subnet" {      # Defines the subnet which the proxy machines will reside in.
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = "true"
+    map_public_ip_on_launch = "true"        # Specifies that the instances launched into this subnet should be assigned a public IP address.
 
     tags = {
       Name = "proxy subnet"
     }
 }
 
-resource "aws_internet_gateway" "proxy_internet_gateway" {
+resource "aws_internet_gateway" "proxy_internet_gateway" {      # Defines the Internet gateway, which allows internet traffic to and from the VPC. This sits outside the subnets, but can only be directly accessed via the proxy subnet.
     vpc_id = aws_vpc.main_vpc.id
 
     tags = {
-      Nmae = "internet gateway for proxies"
+      Name = "internet gateway for proxies"
     }
 }
 
-resource "aws_network_interface" "private_ip_recon" {
+resource "aws_eip" "nat" {      # Provides an elastic (public IP) to the NAT gateway
+  domain = "vpc"                # Indicates if this EIP is for use in VPC
+}
+
+resource "aws_nat_gateway" "nat_gateway" {      # Defines the NAT gateway, which enables instances in private subnets to connect to the internet. It must be deployed in the public subnet with an elastic IP.
+  allocation_id = aws_eip.nat.id                # Attatches the elastic IP as defined from the previous resource.
+  subnet_id = aws_subnet.proxy_subnet.id        # Defines where the NAT gateway is situated (must be in the public subnet i.e. the proxy subnet).
+  depends_on = [aws_internet_gateway.proxy_internet_gateway]      # Adds an explicit dependency on the Internet Gateway for the VPC.
+}
+
+resource "aws_route_table" "public_route_table" {     # Defines which external IPs are accessable from the Internet gateway, or in this case, the proxy subnet (public subnet).
+  vpc_id = aws_vpc.main_vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"                          # Specifies that all IPs are accessable.
+    gateway_id = aws_internet_gateway.proxy_internet_gateway.id 
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {     # Creates an association between a route table and a subnet. So in this case, the route table above relates to the proxy subnet, i.e. the proxy subnet can access all external IPs.
+  subnet_id = aws_subnet.proxy_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {      # Defines which external IPs are accessible from the NAT gateway, i.e. from the recon subnet.
+  vpc_id = aws_vpc.main_vpc
+  route = {
+    cidr_block = "0.0.0.0/0"                            # Specifies that all IPs are accessable from the recon subnet.
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {      # Creates an association between the recon subnet and the private routing table, which specifies that the recon subnet can access the Internet through the NAT gateway.
+  subnet_id = aws_subnet.recon_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_network_interface" "private_ip_recon" {       # Recon machine
     subnet_id = aws_subnet.recon_subnet.id
-    private_ips = ["10.0.0.1"]
+    private_ips = ["10.0.1.1"]
 
     attachment {
       instance = aws_instance.recon.id
@@ -52,9 +88,9 @@ resource "aws_network_interface" "private_ip_recon" {
   }
 }
 
-resource "aws_network_interface" "private_ip_proxy_1" {
+resource "aws_network_interface" "private_ip_proxy_1" {     # Proxy 1
     subnet_id = aws_subnet.proxy_subnet.id
-    private_ips = ["10.0.1.1"]
+    private_ips = ["10.0.2.1"]
 
     attachment {
       instance = aws_instance.proxy_1.id
@@ -65,9 +101,9 @@ resource "aws_network_interface" "private_ip_proxy_1" {
   }
 }
 
-resource "aws_network_interface" "private_ip_proxy_2" {
+resource "aws_network_interface" "private_ip_proxy_2" {     # Proxy 2
     subnet_id = aws_subnet.proxy_subnet.id
-    private_ips = ["10.0.1.2"]
+    private_ips = ["10.0.2.2"]
 
     attachment {
       instance = aws_instance.proxy_2.id
@@ -78,9 +114,9 @@ resource "aws_network_interface" "private_ip_proxy_2" {
   }
 }
 
-resource "aws_network_interface" "private_ip_proxy_3" {
+resource "aws_network_interface" "private_ip_proxy_3" {     # Proxy 3
     subnet_id = aws_subnet.proxy_subnet.id
-    private_ips = ["10.0.1.3"]
+    private_ips = ["10.0.2.3"]
 
     attachment {
       instance = aws_instance.proxy_3.id
@@ -91,9 +127,9 @@ resource "aws_network_interface" "private_ip_proxy_3" {
   }
 }
 
-resource "aws_network_interface" "private_ip_proxy_4" {
+resource "aws_network_interface" "private_ip_proxy_4" {     # Proxy 4
     subnet_id = aws_subnet.proxy_subnet.id
-    private_ips = ["10.0.1.4"]
+    private_ips = ["10.0.2.4"]
 
     attachment {
       instance = aws_instance.proxy_4.id
@@ -104,9 +140,9 @@ resource "aws_network_interface" "private_ip_proxy_4" {
   }
 }
 
-resource "aws_network_interface" "private_ip_proxy_5" {
+resource "aws_network_interface" "private_ip_proxy_5" {     # Proxy 5
     subnet_id = aws_subnet.proxy_subnet.id
-    private_ips = ["10.0.1.5"]
+    private_ips = ["10.0.2.5"]
 
     attachment {
       instance = aws_instance.proxy_5.id
